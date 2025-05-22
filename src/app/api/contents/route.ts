19d7f87f1ee2921a80c729a3a66780ab1b616faf
hansolc <hsc080808@naver.com>
2025-05-23 01:30:31 +0900

    test line delete

--- src/app/api/contents/route.ts
+++ src/app/api/contents/route.ts
@@ -43,7 +43,7 @@ const getContentsData = ({
 export async function GET(request: NextRequest) {
   try {
     // delay for test
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const { searchParams } = new URL(request.url);
     const tab = searchParams.get('tab');
